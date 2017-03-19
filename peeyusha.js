354963b64cce54509b46a2582abb23a6c3f09a4e
peeyusha07rathi <peeyusha07rathi@gmail.com>
2017-03-19 08:24:43 +0530

    peeyusha_one

--- peeyusha.js
+++ peeyusha.js
@@ -1,1 +1,16 @@
-I am  Peeyusha.
+var express = require('express');
+console.log('hey peeyusha');
+
+var server = new express();
+
+server.get('/', function(req,res){
+	res.send('hello you')
+})
+
+server.get('/search', function(req,res){
+	res.send('welcome to search page')
+	})
+
+server.listen(8000,function(){
+	console.log('Listening')
+})
